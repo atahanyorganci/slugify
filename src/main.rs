e29ccba54e03325b3e268cef5bd2dad8b8790360
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-04-15 12:09:08 +0200

    feat: add `clap` with `derive` feature for CLI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-use slug::slugify;
+use clap::Parser;
+use slug;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Cli {
+    input: String,
+    #[clap(short, long)]
+    delim: Option<char>,
+}
+
+fn slugify(input: &str, delim: Option<char>) -> String {
+    let result = slug::slugify(input);
+    match delim {
+        Some(delim) if delim != '-' => result.replace('-', &delim.to_string()),
+        _ => result,
+    }
+}
 
 fn main() {
-    let str = "Hello world";
-    let slug = slugify(str);
-    println!("{str} -> {slug}");
+    let Cli { input, delim } = Cli::parse();
+    if input == "-" {
+        todo!("Read from stdin")
+    } else {
+        println!("{}", slugify(&input, delim));
+    }
 }
